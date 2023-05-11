8f140d5433b679bf2c9ab9bf52478caac298380d
Youki YOSHIOKA <yoshioka.youki@gmail.com>
2023-05-12 00:11:14 +0900

    set local debug settings and implemented msal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,38 @@
 import type { AppProps } from 'next/app'
 
-import { InteractionType } from "@azure/msal-browser";
-import { MsalAuthenticationTemplate, useMsal } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser"
+import { MsalAuthenticationTemplate, MsalProvider, useMsal } from "@azure/msal-react"
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
+import AuthProvider from '@/providers/auth'
+import msalInstance, { loginRequest } from '@/services/msal'
+
+const Header = () => {
+  const { accounts } = useMsal()
+  const username = accounts[0].username
+
   return (
-    <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
-      <Component {...pageProps} />
-    </MsalAuthenticationTemplate>
+    <>
+      Hello, {{ username }} san.
+    </>
+  )
+}
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const authRequest = {
+    ...loginRequest
+  }
+  return (
+    <MsalProvider instance={msalInstance}>
+      <AuthProvider>
+        <MsalAuthenticationTemplate
+          interactionType={InteractionType.Redirect}
+          authenticationRequest={authRequest}
+        >
+          <Header />
+          <Component {...pageProps} />
+        </MsalAuthenticationTemplate>
+      </AuthProvider>
+    </MsalProvider>
   )
 }
 
