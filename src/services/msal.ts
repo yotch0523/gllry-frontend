e4a83d90bc16616b8a7110846531c0e628ce5e15
Youki YOSHIOKA <yoshioka.youki@gmail.com>
2023-05-14 16:50:58 +0900

    implemented authentication

--- src/services/msal.ts
+++ src/services/msal.ts
@@ -2,19 +2,22 @@ import * as msal from '@azure/msal-browser'
 import { Configuration } from '@azure/msal-browser'
 
 const msalConfig: Configuration = {
-    auth: {
-        clientId: process.env.NEXT_PUBLIC_CLIENT_ID ?? '',
-        authority: process.env.NEXT_PUBLIC_AUTHORITY === undefined ? '' : process.env.NEXT_PUBLIC_AUTHORITY,
-        knownAuthorities: process.env.NEXT_PUBLIC_KNOWN_AUTHORITY === undefined ? [] : [process.env.NEXT_PUBLIC_KNOWN_AUTHORITY],
-        redirectUri: '/',
-        postLogoutRedirectUri: '/',
-    }
+  auth: {
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID ?? '',
+    authority: process.env.NEXT_PUBLIC_AUTHORITY === undefined ? '' : process.env.NEXT_PUBLIC_AUTHORITY,
+    knownAuthorities: process.env.NEXT_PUBLIC_KNOWN_AUTHORITY === undefined ? [] : [process.env.NEXT_PUBLIC_KNOWN_AUTHORITY],
+    redirectUri: '/',
+    postLogoutRedirectUri: '/',
+  },
+  cache: {
+    cacheLocation: 'localStorage',
+  },
 }
 export const loginRequest = {
-    scopes: ["openid"]
+  scopes: ["openid", "offline_access"]
 }
 export const apiRequest = {
-    scopes: ['']
+  scopes: ['']
 }
 const msalInstance = new msal.PublicClientApplication(msalConfig)
 export default msalInstance
