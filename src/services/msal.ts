8d8cc38102a555e5df55ab8e3cc0d0883961d013
Youki YOSHIOKA <yoshioka.youki@gmail.com>
2023-05-26 00:03:30 +0900

    implemented api request with msal auth

--- src/services/msal.ts
+++ src/services/msal.ts
@@ -1,6 +1,12 @@
 import * as msal from '@azure/msal-browser'
 import { Configuration } from '@azure/msal-browser'
 
+const apiScopes: string[] = []
+if (process.env.NEXT_PUBLIC_IMAGE_API_READ_SCOPE)
+  apiScopes.push(process.env.NEXT_PUBLIC_IMAGE_API_READ_SCOPE)
+if (process.env.NEXT_PUBLIC_IMAGE_API_WRITE_SCOPE)
+  apiScopes.push(process.env.NEXT_PUBLIC_IMAGE_API_WRITE_SCOPE)
+
 const msalConfig: Configuration = {
   auth: {
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID ?? '',
@@ -19,11 +25,9 @@ const msalConfig: Configuration = {
     cacheLocation: 'localStorage',
   },
 }
-export const loginRequest = {
+export const loginRequest: msal.RedirectRequest = {
   scopes: ['openid', 'offline_access'],
 }
-export const apiRequest = {
-  scopes: [''],
-}
+
 const msalInstance = new msal.PublicClientApplication(msalConfig)
 export default msalInstance
